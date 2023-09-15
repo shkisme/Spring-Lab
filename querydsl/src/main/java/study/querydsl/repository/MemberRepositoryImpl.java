8284196c64d585d78d680a7f111685e3a17c824e
shkisme <shkisme0130@gmail.com>
2023-09-15 20:15:12 +0900

    [querydsl] 스프링 데이터 페이징 활용

--- querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -4,14 +4,21 @@ import static io.micrometer.common.util.StringUtils.isEmpty;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
@@ -54,5 +61,105 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
   private BooleanExpression ageLoe(Integer ageLoe) {
     return ageLoe != null ? member.age.loe(ageLoe) : null;
+  }
+
+  /**
+   * 단순 페이징, fetchResults() 사용
+   */
+  @Override
+  public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+    QueryResults<MemberTeamDto> results = queryFactory
+        .select(new QMemberTeamDto(
+            member.id,
+            member.username,
+            member.age,
+            team.id,
+            team.name
+        ))
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageGoe(condition.getAgeGoe()),
+            ageLoe(condition.getAgeLoe()))
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetchResults(); // 내용과 전체 카운트를 한번에 조회 가능.
+    // .fetchResult()는 카운트 실행시 필요없는 Order by 제거함.
+
+    List<MemberTeamDto> content = results.getResults();
+    long total = results.getTotal();
+    return new PageImpl<>(content, pageable, total);
+  }
+
+  /**
+   * 데이터 내용과 전체 카운트를 별도로 조회하는 방법 / 복잡한 페이징 / 데이터 조회 쿼리와, 전체 카운트 쿼리를 분리
+   */
+
+  @Override
+  public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+    List<MemberTeamDto> content = queryFactory
+        .select(new QMemberTeamDto(
+            member.id,
+            member.username,
+            member.age,
+            team.id,
+            team.name
+        ))
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageGoe(condition.getAgeGoe()),
+            ageLoe(condition.getAgeLoe()))
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetch();
+
+    long total = queryFactory
+        .select(member)
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageGoe(condition.getAgeGoe()),
+            ageLoe(condition.getAgeLoe()))
+        .fetchCount();
+    return new PageImpl<>(content, pageable, total);
+  }
+
+  /**
+   * CountQuery 최적화 / PageableExecutionUtils.getPage()로 최적화.
+   */
+  public Page<MemberTeamDto> searchPageComplex2(MemberSearchCondition condition, Pageable pageable) {
+    List<MemberTeamDto> content = queryFactory
+        .select(new QMemberTeamDto(
+            member.id,
+            member.username,
+            member.age,
+            team.id,
+            team.name
+        ))
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageGoe(condition.getAgeGoe()),
+            ageLoe(condition.getAgeLoe()))
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetch();
+
+    JPAQuery<Member> countQuery = queryFactory
+        .select(member)
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageGoe(condition.getAgeGoe()),
+            ageLoe(condition.getAgeLoe()));
+    return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
+    // 스프링 데이터 라이브러리가 제공.
+    // count 쿼리가 생략 가능한 경우에 생략해서 처리함.
   }
 }
