72776d063ac259e27f4137d532aebdeb5c569e58
shkisme <shkisme0130@gmail.com>
2023-09-15 20:01:34 +0900

    [querydsl] querydsl을 적용한 커스텀 레포지토리 동작 테스트 작성

--- querydsl/src/test/java/study/querydsl/MemberJpaRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/MemberJpaRepositoryTest.java
@@ -15,6 +15,7 @@ import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.Team;
 import study.querydsl.repository.MemberJpaRepository;
+import study.querydsl.repository.MemberRepository;
 
 @SpringBootTest
 @Transactional
@@ -25,6 +26,9 @@ public class MemberJpaRepositoryTest {
 
   @Autowired
   MemberJpaRepository memberJpaRepository;
+
+  @Autowired
+  MemberRepository memberRepository;
 
   @Test
   @DisplayName("basicTest")
@@ -82,6 +86,34 @@ public class MemberJpaRepositoryTest {
     condition.setTeamName("teamB");
 
     List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+
+    assertThat(result).extracting("username").containsExactly("member4");
+  }
+
+  @Test
+  @DisplayName("커스텀 레포지토리 동작 테스트")
+  public void 커스텀_레포지토리_동작_테스트() throws Exception {
+    Team teamA = new Team("teamA");
+    Team teamB = new Team("teamB");
+    em.persist(teamA);
+    em.persist(teamB);
+
+    Member member1 = new Member("member1", 10, teamA);
+    Member member2 = new Member("member2", 20, teamA);
+    Member member3 = new Member("member3", 30, teamB);
+    Member member4 = new Member("member4", 40, teamB);
+
+    em.persist(member1);
+    em.persist(member2);
+    em.persist(member3);
+    em.persist(member4);
+
+    MemberSearchCondition condition = new MemberSearchCondition();
+    condition.setAgeGoe(35);
+    condition.setAgeLoe(40);
+    condition.setTeamName("teamB");
+
+    List<MemberTeamDto> result = memberRepository.search(condition);
 
     assertThat(result).extracting("username").containsExactly("member4");
   }
